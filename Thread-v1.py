0e637355b3ac70027d94da9f85ae69f02f494120
hch-cc <60416372+hch-cc@users.noreply.github.com>
2023-10-04 15:56:20 +0800

    Update Thread-v1.py

--- Thread-v1.py
+++ Thread-v1.py
@@ -98,7 +98,7 @@ def myThread(q, q1, lock, header):
             lock.acquire()  # 锁住
             data.append(info)
             lock.release()  # 释放
-            # get_p(u + "comments", order, lock, header) # 这是获取图片的
+            # get_p(u + "comments", order, lock, header) # 这是获取图片的,需要照片的可以去掉注释，建议用多进程版本的
             print("第", order)
 
         else:
